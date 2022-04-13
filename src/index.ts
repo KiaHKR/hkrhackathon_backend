45ccb51388bbd38b65a8494a143236c02db3fcc9
lekatehdas <aki.sirkia@gmail.com>
2022-04-13 12:07:04 +0200

    added users route to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+import users from './routes/users'
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -9,6 +10,7 @@ if (!process.env.JWT_KEY) {
 }
 
 app.use(express.json());
+app.use('/users', users);
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
