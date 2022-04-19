473e38eeca293d92cf9fb91bd1349291c0cafa03
lekatehdas <aki.sirkia@gmail.com>
2022-04-19 23:01:34 +0200

    Created Auth middleware.

---
+++ src/middleware/auth.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+module.exports = function (req, res, next) {
+    const token = req.header('x-auth-header');
+    if(!token) return res.status(401).send('Access denied. No token provided.');
+
+    try {
+        req.user = jwt.verify(token, process.env.JWT_KEY);
+        next();
+    }
+    catch (ex) {
+        res.status(400).send('Invalid token.')
+    }
+}
