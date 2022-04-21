df476b0d7887b59706bd888775672caa79615e83
Tom <welter.tom@hotmail.com>
2022-04-21 21:41:49 +0200

    changed functions to async

--- src/database/dbhandler.ts
+++ src/database/dbhandler.ts
@@ -1,55 +1,55 @@
 import { User } from "../models/user" // userObject
 export class dbhandler {
 
-    saveUserObject(user) {
+    async saveUserObject(user) {
         // saves a user to the database.
         console.log("saveUserObject confirm")
     }
 
-    getUserObject(email) {
+    async getUserObject(email) {
         // fetches a single user from the database and sends an object back.
         console.log("getUserObject confirm")
         return new User("joe", "joe@mama.se", "joehasnomama", 2022)
     }
 
-    getAllUserObject() {
+    async getAllUserObject() {
         // returns an array of all users in the database
         console.log("getAllUserObject confirm")
         return [new User("joe", "joe@mama.se", "joehasnomama", 2022), new User("john", "john@doe.se", "johndoebae", 2022)]
     }
 
-    deleteUserObject(email) {
+    async deleteUserObject(email) {
         // deletes user from the database, ADMIN ONLY
         console.log("deleteUserObject confirm")
     }
 
-    updateUserObject(user) {
+    async updateUserObject(user) {
         // updates a user in the database.
         console.log("updateUserObject confirm")
     }
 
     // Puzzle related calls
-    savePuzzle(puzzle) {
+    async savePuzzle(puzzle) {
         // Saves a puzzle to the database.
         console.log("savePuzzle confirm")
     }
 
-    getPuzzle(puzzleId) {
+    async getPuzzle(puzzleId) {
         // returns a puzzle from the database.
         console.log("getPuzzle confirm")
     }
 
-    getAllPuzzles() {
+    async getAllPuzzles() {
         // returns an array of all users from the database.
         console.log("getAllPuzzles confirmed")
     }
 
-    updatePuzzle(puzzle) {
+    async updatePuzzle(puzzle) {
         // updates a puzzle in the database.
         console.log("updatePuzzle confirmed")
     }
 
-    deletePuzzle(id) {
+    async deletePuzzle(id) {
         // deletes a puzzle from the database.
         console.log("detelePuzzle confirmed")
     }
