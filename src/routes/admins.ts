445994106df18444453ead4b2903d9654b08f104
lekatehdas <aki.sirkia@gmail.com>
2022-04-21 22:52:41 +0200

    Added all the route functionality to the admin route.

--- src/routes/admins.ts
+++ src/routes/admins.ts
@@ -11,7 +11,6 @@ const db = new dbhandler;
 // DELETE user.
 router.delete('/:email', [auth, admin], async (req, res) => {
     const user = await db.deleteUserObject(req.params.email);
-
     if (!(user instanceof User)) return res.status(404).json({ error: "Email not found."});
 
     res.send(user);
@@ -19,19 +18,34 @@ router.delete('/:email', [auth, admin], async (req, res) => {
 
 // UPDATE user.
 router.put('/:email', [auth, admin], async (req, res) => {
-    // validate input, if not valid return 400.
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).json({error: error.details[0].message});
 
-    // dbHandler: GET userObject. If not found, return error.
+    let user: User = await db.getUserObject(req.params.email);
+    if (!(user instanceof User)) return res.status(404).json({ error: "Email not found."});
+    user.name = req.body.name;
+    user.password = req.body.password;
+    user.year = req.body.year;
+
+    user = await db.updateUserObject(user);
+
+    res.status(200).send(user);
 });
 
 // GET user.
 router.get('/:email', [auth, admin], async (req, res) => {
-    // dbHandler: GET userObject. If not found, return error.
+    const user = await db.getUserObject(req.params.email);
+    if (!(user instanceof User)) return res.status(404).json({ error: "Email not found."});
+
+    res.send(user);
 });
 
 // GET all the users
 router.get('/', [auth, admin], async (req, res) => {
-    // dbHandler: GET ALL userObjects.
+    const users = await db.getAllUserObject();
+    if (users.length === 0) return res.status(404).json({error: "No users in the database."});
+
+    res.status(200).send(users);
 });
 
 export = router;
