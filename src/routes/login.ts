0356a2bccb63e0e7636771bfd37b1683a85b7ad9
lekatehdas <aki.sirkia@gmail.com>
2022-04-21 22:00:21 +0200

    Finished login route. Edited 'users' route ro 'user' in index.

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { User } from "../models/user";
+import { dbhandler } from "../database/dbhandler";
 import bcrypt from 'bcrypt';
 import Joi from "joi";
 
@@ -9,11 +9,15 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).json({error: error.details[0].message});
 
-    // dbHandler: GET userObject. If not found, return error.
+    const db = new dbhandler;
+    const user = await db.getUserObject(req.body.email);
+    if (!user) return res.status(400).json({ error: "Invalid email or password."});
 
-    // validate password by using compare from bcrypt. If not valid return error.
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-    // create token and return it to the user.
+    const userToken = user.generateAuthToken();
+    res.json({ token: userToken });
 });
 
 function validate(request) {
@@ -21,7 +25,6 @@ function validate(request) {
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(8).max(50).required()
     });
-
     return schema.validate(request);
 }
 
