3516bfb61d6577416740de5b9f538319d29232c6
lekatehdas <aki.sirkia@gmail.com>
2022-04-23 23:01:08 +0200

    Edited admins route to return publicUser.

--- src/routes/admins.ts
+++ src/routes/admins.ts
@@ -6,13 +6,17 @@ import { dbhandler } from "../database/dbhandler";
 import { User, validateUserUpdate } from '../models/user';
 const db = new dbhandler;
 import asyncMiddleware from '../middleware/async'
+import PublicUser from "../models/publicUser";
 
 // DELETE user.
 router.delete('/:email', [auth, admin], asyncMiddleware(async (req, res) => {
     const user = await db.deleteUserObject(req.params.email);
     if (!(user instanceof User)) return res.status(404).json({ error: "Email not found."});
 
-    res.send(user);
+    const publicUser = new PublicUser;
+    publicUser.fromUser(user)
+    
+    res.status(200).send(publicUser);
 }));
 
 // UPDATE user.
@@ -28,7 +32,10 @@ router.put('/:email', [auth, admin], asyncMiddleware(async (req, res) => {
 
     user = await db.updateUserObject(user);
 
-    res.status(200).send(user);
+    const publicUser = new PublicUser;
+    publicUser.fromUser(user)
+
+    res.status(200).send(publicUser);
 }));
 
 // GET user.
@@ -36,15 +43,25 @@ router.get('/:email', [auth, admin], asyncMiddleware(async (req, res) => {
     const user = await db.getUserObject(req.params.email);
     if (!(user instanceof User)) return res.status(404).json({ error: "Email not found."});
 
-    res.send(user);
+    const publicUser = new PublicUser;
+    publicUser.fromUser(user)
+
+    res.status(200).send(publicUser);
 }));
 
 // GET ALL users
 router.get('/', [auth, admin], asyncMiddleware(async (req, res) => {
-    const users = await db.getAllUserObject();
+    const users: User[] = await db.getAllUserObject();
     if (users.length === 0) return res.status(404).json({error: "No users in the database."});
-
-    res.status(200).send(users);
+    
+    const publicUsers = [];
+    users.forEach((user) => {
+        const publicUser = new PublicUser;
+        publicUser.fromUser(user)
+        publicUsers.push(publicUser);
+    });
+    
+    res.status(200).send(publicUsers);
 }));
 
 // POST PUZZLES
