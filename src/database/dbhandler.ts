c61c061b99878a767c588e0ffac183a5a19b33ed
tom <welter.tom@hotmail.com>
2022-04-27 07:37:17 +0200

    Added functionality to puzzle related db calls.

--- src/database/dbhandler.ts
+++ src/database/dbhandler.ts
@@ -21,7 +21,11 @@ export class dbhandler {
         return { name: user._name, email: user._email, password: user._password, year: user._year, currentTask: user._currentTask, userPuzzles: user._userPuzzles, isAdmin: user._isAdmin }
     }
 
-    async saveUserObject(user) { // tested and working, missing existing check
+    puzzleDeconstruct(puzzle) {
+       return { id:puzzle._id, title:puzzle._title, story:puzzle._story, examples:puzzle._examples, template:puzzle._template, answer:puzzle._answer }
+    }
+
+async saveUserObject(user) { // tested and working, missing existing check
         const userInfo = this.userDeconstruct(user)
         const newUser = new dbUser(userInfo
         )
@@ -74,27 +78,50 @@ export class dbhandler {
     // Puzzle related calls
     async savePuzzle(puzzle) {
         // Saves a puzzle to the database.
+        const puzzleData = this.puzzleDeconstruct(puzzle)
+        const newPuzzle = new dbPuzzle(puzzleData)
+        await newPuzzle.save();        
         console.log("savePuzzle confirm.")
+        return newPuzzle
     }
 
     async getPuzzle(puzzleId) {
         // returns a puzzle from the database.
-        console.log("getPuzzle confirm.")
+        const puzzle = await dbPuzzle.findOne({ id:puzzleId })
+        if (puzzle) {
+            console.log("getPuzzle confirm.")
+            return puzzle
+        } else {
+            console.log("getPuzzle confirm.")
+            return 'Puzzle not found.'
+        }
     }
+    
 
     async getAllPuzzles() {
         // returns an array of all users from the database.
-        console.log("getAllPuzzles confirmed.")
+        const puzzles = await dbPuzzle.find();
+        console.log("getAllPuzzles confirm.");
+        return puzzles
     }
 
     async updatePuzzle(puzzle) {
-        // updates a puzzle in the database.
-        console.log("updatePuzzle confirmed.")
+        const puzzleData = this.puzzleDeconstruct(puzzle)
+        const res = await dbPuzzle.findOneAndUpdate({ id: puzzleData.id }, { title:puzzleData.title, story:puzzleData.story, examples:puzzleData.examples, template:puzzleData.template, answer:puzzleData.answer });
+        if (res) {
+            console.log("updatePuzzle confirm.")
+            return res
+        } else {
+            console.log("updatePuzzle confirm.")
+            return "Puzzle update failed, puzzle not found."
+        }
     }
 
-    async deletePuzzle(id) {
+    async deletePuzzle(puzzleid) {
         // deletes a puzzle from the database.
-        console.log("detelePuzzle confirmed.")
+        const confirmation = await dbPuzzle.deleteOne({ id: puzzleid })
+        console.log("deletePuzzle confirmed.")
+        return confirmation.deletedCount
     }
 
     async getNextPuzzleId(id?: string) {
