2a512476157e9ac77d11c3b4ad1964a04017b879
lekatehdas <aki.sirkia@gmail.com>
2022-04-27 11:06:09 +0200

    Added ID check for the methods.

--- src/puzzle_service/puzzleHandler.ts
+++ src/puzzle_service/puzzleHandler.ts
@@ -10,12 +10,18 @@ export default class PuzzleHandler{
     };
 
     static generatePuzzle(id: string): UserPuzzle {
+        if (!this.idFound(id)) throw new Error("ID does not match to any puzzle modules");
         const puzzleClass = PuzzleHandler.puzzleClasses[id];
         return puzzleClass.generatePuzzle();
     }
 
     static checkAnswer(id: string, correctAnswer: string, guessAnswer: string) {
+        if (!this.idFound(id)) throw new Error("ID does not match to any puzzle modules");
         const puzzleClass = PuzzleHandler.puzzleClasses[id];
         return puzzleClass.checkAnswer(correctAnswer, guessAnswer);
+    }
+
+    static idFound(id: string) {
+        return (id in this.puzzleClasses);
     }
 }
