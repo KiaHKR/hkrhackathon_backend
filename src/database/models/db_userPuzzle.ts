6e675cbd2f9e690f1c7db5157920715a9f6ec243
Tom <welter.tom@hotmail.com>
2022-04-30 11:25:07 +0200

    Added userPuzzle for structure

---
+++ src/database/models/db_userPuzzle.ts
@@ -0,0 +1,35 @@
+import { boolean } from 'joi';
+import mongoose from 'mongoose';
+var userPuzzleSchema = new mongoose.Schema({
+
+    userInput: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 255
+    },
+    answer: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 255
+    },
+    completionTime: {
+        type: Number,
+        required: true,
+        min: 3,
+        max: 255
+    },
+    numberOfWrongSubmissions: {
+        type: Number
+
+    },
+    completed: {
+        type: Boolean
+    }
+
+
+
+})
+const dbUserPuzzle = mongoose.model('UserPuzzle', userPuzzleSchema)
+export { dbUserPuzzle }
