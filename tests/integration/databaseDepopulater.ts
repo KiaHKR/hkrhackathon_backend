10d4b920c927d1c5a90b864fbc83ed58e6f6fce3
Tom <welter.tom@hotmail.com>
2022-05-01 12:30:06 +0200

    removed dbUserPuzzle related functions

--- tests/integration/databaseDepopulater.ts
+++ tests/integration/databaseDepopulater.ts
@@ -1,9 +1,7 @@
-import {dbUser} from "../../src/database/models/db_users";
-import {dbUserPuzzle} from "../../src/database/models/db_userPuzzle";
-import {dbPuzzle} from "../../src/database/models/db_puzzles";
+import { dbUser } from "../../src/database/models/db_users";
+import { dbPuzzle } from "../../src/database/models/db_puzzles";
 
 export async function depopulateDatabase() {
     await dbUser.remove();
-    await dbUserPuzzle.remove();
     await dbPuzzle.remove();
 }
