7ff67d598fe7db287437832a038d9e8739975723
Tom <welter.tom@hotmail.com>
2022-05-03 21:46:02 +0200

    added/modified methods to change puzzle storage

--- src/database/puzzleHandlerDB.ts
+++ src/database/puzzleHandlerDB.ts
@@ -35,58 +35,55 @@ export class PuzzleHandlerDB {
         // returns an array of all users from the database.
         const puzzles = await dbPuzzle.find();
         let puzzleList = [];
-        if (puzzles.length == 0) {
-            return { error: "No puzzles in database." }
-        } else {
-            for (const i of puzzles) {
-                puzzleList.push(this.puzzleReconstruct(i))
-            }
-            return puzzleList
+        if (puzzles.length == 0) return { error: "No puzzles in database." }
 
+        for (const i of puzzles) {
+            puzzleList.push(this.puzzleReconstruct(i))
         }
+        return puzzleList
+
+
     }
 
     async getNextPuzzleId(id?: string): Promise<string | { error: string; }> {
         // if you get an argument, return right puzzle. Else, first puzzle.
-        const puzzles = await dbpuzzleStorage.find({ visibility: true }).sort({ "puzzleid": 1 });
-        if (puzzles.length !== 0) {
-            if (id == undefined) {
-                return puzzles[0].id
-            } else {
-                let next = false;
-                for (let i of puzzles) {
-                    if (next) {
-                        return i.id
-                    }
-                    if (i.id == id) {
-                        next = true;
-                    }
-                }
-
+        const orderArray = await dbpuzzleStorage.find();
+        const returnPuzzle = []
+        if (orderArray.length == 0) return { error: "orderArray missing." }
+        for (const puzzle of orderArray) {
+            if (puzzle.visiblity) {
+                returnPuzzle.push(puzzle.puzzleid)
             }
-        } else { return { error: "Error while browsing Puzzles." } }
-
-    }
-
-    async changePuzzleVisibility(id: string, visiblity: boolean): Promise<string | { error: string; }> {
-        const puzzle = await dbpuzzleStorage.findOne({ id: id });
-        if (puzzle) {
-            puzzle.public = visiblity;
-            return puzzle.id
-        } else { return { error: "No such puzzle found." } }
-    }
-
-    async addPuzzleToStorage(id: string, visiblity: boolean): Promise<string | { error: string; }> {
-        const check = await dbpuzzleStorage.findOne({ id: id });
-        if (check) { return { error: "Puzzle exists already." } }
-        else {
-            const puzzle = await new dbpuzzleStorage({ id: id, public: visiblity })
-            await puzzle.save()
-            return puzzle.id
+        }
+        if (returnPuzzle.length == 0) { return { error: "Error while browsing Puzzles." } }
+        if (id == undefined) { return returnPuzzle[0] }
+        let next = false;
+        for (let i of returnPuzzle) {
+            if (next) { return i }
+            if (i == id) {
+                next = true;
+            }
         }
     }
 
-    async delPuzzleFromStorage(id: string): Promise<string | { error: string; }> {
+    async updatingPuzzleStorage(orderArray: [{ id: string, visiblity: boolean }]): Promise<[{ id: string, visiblity: boolean }]> {
+        // updates puzzle storage array
+        return await new dbpuzzleStorage(orderArray).save()
+    }
+}
+
+    /* async changePuzzleVisibility(id: string, visiblity: boolean): Promise<string | { error: string; }> {
+         const puzzle = await dbpuzzleStorage.findOne({ puzzleid: id });
+         if (puzzle) {
+             puzzle.public = visiblity;
+             return puzzle.puzzleid
+         } else { return { error: "No such puzzle found." } }
+        }
+    */
+
+
+
+    /*async delPuzzleFromStorage(id: string): Promise<string | { error: string; }> {
         const puzzle = await dbpuzzleStorage.findOne({ id: id });
         if (puzzle) {
             const puzzleRet = await puzzle.deleteOne({ id: id })
@@ -98,5 +95,6 @@ export class PuzzleHandlerDB {
 
         }
     }
+    */
 
 }
