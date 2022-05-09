5ccf1aff45aff490cfe8c76a0705413c8173b9a5
lekatehdas <aki.sirkia@gmail.com>
2022-05-09 10:12:59 +0200

    Heroku push.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import express from 'express';
 const app = express();
 
-import logger from "./utility_services/logger";
+import logger from "./utility_services/logger.js";
 import config from "./startup/config";
 import logging from "./startup/logging";
 import db from './startup/db'
