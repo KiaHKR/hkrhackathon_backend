347d500163cc72efe40cc9668ac2cf319efa11d2
lekatehdas <aki.sirkia@gmail.com>
2022-05-09 11:48:24 +0200

    Making build to work. Work in progress.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import express from 'express';
 const app = express();
 
-import logger from "./utility_services/logger.js";
+import logger from "./utility_services/logger";
 import config from "./startup/config";
 import logging from "./startup/logging";
 import db from './startup/db'
