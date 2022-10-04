04d26c3bc6a20b8d7fc7be20ef21cdc7c5d29df6
lekatehdas <aki.sirkia@gmail.com>
2022-10-04 14:42:37 +0200

    testing the nice strings

--- src/puzzle_service/2022_1/03_nice_words/constants.ts
+++ src/puzzle_service/2022_1/03_nice_words/constants.ts
@@ -1,5 +1,5 @@
 export default class Constants {
-    static NUMBER_OF_STRINGS = 2000;
+    static NUMBER_OF_STRINGS = 200000;
     static MIN_STRING_LENGTH = 5;
     static MAX_STRING_LENGTH = 15;
     static STRING_CHARS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
