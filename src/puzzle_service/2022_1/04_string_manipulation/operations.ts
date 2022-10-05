cb5d49d6b0aa2451e9e9003260f2ba1d82314509
Lasse Poulsen <work.lasse.face@gmail.com>
2022-10-06 00:33:26 +0200

    Change string manip puzzle

--- src/puzzle_service/2022_1/04_string_manipulation/operations.ts
+++ src/puzzle_service/2022_1/04_string_manipulation/operations.ts
@@ -1,6 +1,12 @@
 const performRotate = (s: string, count: number): { s: string, op_string: string } => {
     const op_string = `r${count}`;
-    return { s: s.substring(s.length - count % s.length) + s.substring(0, s.length - count % s.length), op_string: op_string };
+    let s_arr = s.split('');
+
+    const part_to_move_to_front = s_arr.slice(s_arr.length - count % s_arr.length);
+    const part_to_move_to_back = s_arr.slice(0, s_arr.length - count % s_arr.length);
+
+    const output_arr = part_to_move_to_front.concat(part_to_move_to_back);
+    return { s: output_arr.join(''), op_string: op_string };
 };
 
 const performSwap = (s: string, index_a: number, index_b: number): { s: string, op_string: string } => {
@@ -27,9 +33,15 @@ const performExchange = (s: string, item_a: string, item_b: string): { s: string
 
 const performShift = (s: string, item_a: string): { s: string, op_string: string } => {
     const op_string = `s${item_a}`;
-    const item_index = (s.indexOf(item_a) + 3) % (s.length - 1);
-    s = s.replace(item_a, '');
-    return { s: s.substring(0, item_index) + item_a + s.substring(item_index), op_string: op_string };
+    let s_arr = s.split('');
+
+    let item_index = (s_arr.indexOf(item_a) + 3);
+    if (item_index >= s_arr.length) item_index = s_arr.length - 1;
+
+    s_arr.splice(s_arr.indexOf(item_a), 1);
+    s_arr.splice(item_index, 0, item_a);
+
+    return { s: s_arr.join(''), op_string: op_string };
 };
 
 const performMirror = (s: string): { s: string, op_string: string } => {
